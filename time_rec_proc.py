a49b468653c3dbd306dcf577a395250006bc2938
Corvit Chr <corvit.christian@gmail.com>
2018-05-24 10:10:17 +0300

    qtimerec_2018_03: Added 2 strings (with year 2017) at the beginning for test purposes
    yaml file: added stored_filename for stored base of previously imported data
    
    main .py file:
      1) added get_abs_path()
      2) added load_stored_base() that currently only checks if stored base file exists
      3) added new field "Year" for future data storage separated in sheets by year

--- time_rec_proc.py
+++ time_rec_proc.py
@@ -36,10 +36,7 @@ def logging_setup():
 
 
 def load_cfg():
-    """
-
-    :rtype: tuple
-    """
+    """Loads configuration file and initializes variables"""
     # importing configuration
     yaml_name = path.splitext(filename)[0] + ".yaml"
     with codecs.open(full_path + "/" + yaml_name, 'r', encoding='utf-8') as yaml_file:
@@ -64,13 +61,14 @@ def load_cfg():
     logger.debug("Dirs: Input: {0} || Output: {1}".format(inp_dir, outp_dir))
 
     # process csv related parameters
-    global imp_columns, subst_month, subst_other, csv_sep, subst_items
+    global imp_columns, subst_month, subst_other, csv_sep, subst_items, stored_base_filename
 
     imp_columns = cfg['csv_parameters']['columns']
     subst_month = cfg['csv_parameters']['substs']['months']
     subst_items = cfg['csv_parameters']['substs']['items']
     subst_other = cfg['csv_parameters']['substs']['other']
     csv_sep = cfg['csv_parameters']['separator']
+    stored_base_filename = cfg['stored_filename']
     logger.debug("Substs: \nColumns: {0} || \nMonths: {1} || \nOther: {2} || \nCSV Separator: {3} || \nItems: {4}".format(imp_columns, subst_month, subst_other, csv_sep, subst_items))
 
     #return src_dir, dst_dir
@@ -79,11 +77,13 @@ def load_cfg():
 def imp_df(src_dir):
     """importing pandas dataframe from csv file"""
 
-    #global full_path
+    src_dir = get_abs_path(src_dir, full_path)
+    """
     if not path.isabs(src_dir):
         src_dir = full_path + '/' + src_dir
         logger.debug("Path is relative")
     logger.debug("Input dir is: {0}|| Full path is: {1}".format(src_dir, full_path))
+    """
 
     df_imported = pd.read_csv(src_dir + "qtimerec_2018_03.csv", encoding='utf-8', sep=csv_sep, header=0, names=imp_columns)
     df_imported = df_imported.dropna()
@@ -103,6 +103,28 @@ def imp_df(src_dir):
 
     return df_imported
 
+def load_stored_base():
+    """function to load all previously processed data"""
+
+    global base_exists#, stored_base_filename
+    base_exists = False
+
+    base_filename = get_abs_path(stored_base_filename, full_path)
+    if path.exists(base_filename):
+        base_exists = True
+
+    logger.debug("Base exists: %s", base_exists)
+
+
+def get_abs_path(src_path, fullpath):
+    """Checks whether path is absolute or relative and returns absolute path"""
+
+    if not path.isabs(src_path):
+        src_path = fullpath + '/' + src_path
+        logger.debug("Path was relative. Converted to '%s'", src_path)
+
+    return src_path
+
 # main starts here
 logger = logging_setup()
 
@@ -112,12 +134,20 @@ logger.debug("Full path: {0} | filename: {1}".format(full_path, filename))
 
 load_cfg()
 
+load_stored_base()
+
 df_imported = imp_df(inp_dir)
+
+# add field Year and group by year
+df_imported['Year'] = df_imported[imp_columns[0]].dt.year
+#df_imported = df_imported.groupby('Year', as_index=True).agg({"Task":"sum"})
+logger.debug("\nImported DF with 'Year' field added:\n{0}".format(df_imported.head()))
 
 # TODO 1) export/store .xlsx file with years as sheet names
 # TODO 2) imported file to be merged with stored file making sure that dates range was not in stored file before
 # TODO 2) a) perhaps we need to store index of date ranges in a separate sheet?
-# TODO 3) add new field - year. Store data in separate sheets by year
+# (/) TODO 3) add new field - year.
+# TODO 3) a)Store data in separate sheets by year
 # TODO 4) Reading stored data: a) list sheets b) number by years c) read into array of DFs
 
 logger.debug("That's all folks")
