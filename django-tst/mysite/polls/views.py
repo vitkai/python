faba1f8613056a1a3d9bcd21b8f5a85b22631572
Corvit Chr <corvit.christian@gmail.com>
2018-08-16 17:23:18 +0300

    django tutorial: updated pools urls and views with question_id details

--- django-tst/mysite/polls/views.py
+++ django-tst/mysite/polls/views.py
@@ -4,3 +4,16 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hi there! You are at the polls index.")
 
+
+def detail(request, question_id):
+    return HttpResponse("Your are looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    result = "Your are looking at the results of question %s."
+    return HttpResponse(result % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Your are voting on question %s" % question_id)
+
