757800745fc6a9ec132422171c3b1690b6b390d4
Corvit Chr <corvit.christian@gmail.com>
2018-08-17 23:07:56 +0300

    django tutorial: added templates in for views + content to display

--- django-tst/mysite/polls/views.py
+++ django-tst/mysite/polls/views.py
@@ -1,8 +1,15 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hi there! You are at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = { 'latest_question_list': latest_question_list, }
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    #return HttpResponse("Latest question list:\n%s" % output)
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
