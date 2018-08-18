d1cb3ae1e1ea9c3c42412807884ccdbe973a0b40
Corvit Chr <corvit.christian@gmail.com>
2018-08-18 18:21:05 +0300

    django tutorial: added results.html+view and votes counting

--- django-tst/mysite/polls/views.py
+++ django-tst/mysite/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404, HttpResponse
-from .models import Question
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -14,10 +16,20 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    result = "Your are looking at the results of question %s."
-    return HttpResponse(result % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
 
 
 def vote(request, question_id):
-    return HttpResponse("Your are voting on question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': "You didn't select a choice.",  })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+    # Always return HttpResponseRedirect after successfully dealing with POST data to prevent double posting of data if a user hits Back
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
