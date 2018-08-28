2f1e65b76a80dcc409a86199ab4b3e06a5aa005b
Corvit Chr <corvit.christian@gmail.com>
2018-08-28 17:05:05 +0300

    django tutorial: questions view in /admin updated

--- django-tst/mysite/polls/admin.py
+++ django-tst/mysite/polls/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Choice, Question
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
