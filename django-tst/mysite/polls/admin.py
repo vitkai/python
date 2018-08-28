945b4e290e39ae24781e24f1c26dc84ac7897479
Corvit Chr <corvit.christian@gmail.com>
2018-08-28 17:18:47 +0300

    django tutorial: admin published_recently outlook updated

--- django-tst/mysite/polls/admin.py
+++ django-tst/mysite/polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -13,6 +13,7 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information',    {'fields': ['pub_date'], 'classes':['collapse']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 admin.site.register(Question, QuestionAdmin)
 #admin.site.register(Choice)
