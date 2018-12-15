6e2432256d7cd3b31e41bdddef02d1a89eac424d
Corvit Chr <corvit.christian@gmail.com>
2018-12-15 23:15:14 +0300

    test_tasks: initial commit

---
+++ test_tasks/drgn_crvs.py
@@ -0,0 +1,11 @@
+"""
+2018-12-15
+Dragon curves can be got by folding strip of paper n times
+They can be described as set of moves F(orward) and L(eft)/R(ight) turns
+Each n curve contains n-1 curve + L + reversed curve
+Samples:
+n=1 : F
+n=2 : FLF
+n=3 : FLF L FRF
+n=4 : FLF L FRF L FRF L FLF
+"""
