28232c3cbb808eb844a809928b67bdc5a006bffd
Corvit <corvit.christian@gmail.com>
2018-12-29 09:16:55 +0200

    goss_spread.py: initial placeholder commit

---
+++ test_tasks/goss_spread.py
@@ -0,0 +1,6 @@
+"""
+takes N party possible participants
+1st invitee passes info to a random candidate but not to the host
+all other invitees pass info to another one but the one he get info from or host
+to find out if info reaches all of the possible guests
+"""
