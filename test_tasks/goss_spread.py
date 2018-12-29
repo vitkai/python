166ce6b89e8c6c48483e7ca66979f8afc07325cc
Corvit <corvit.christian@gmail.com>
2018-12-29 09:53:26 +0200

    goss_spread.py: minor updates

--- test_tasks/goss_spread.py
+++ test_tasks/goss_spread.py
@@ -4,3 +4,17 @@ takes N party possible participants
 all other invitees pass info to another one but the one he get info from or host
 to find out if info reaches all of the possible guests
 """
+import random
+N = 10
+
+def pass_goss(src):
+    r = random.random(1, N)
+    print (r)
+    return dst
+    
+
+# init arr of N
+guests = [0] * N
+guests[0:1] = [1, 1] # host and 1st invitee know
+    
+print(guests)
