408015e8cce289d7cae0c1e8eeda29dcc847ca32
Corvit Chr <corvit.christian@gmail.com>
2019-12-15 15:44:56 +0300

    HBP: adding tables into db

--- HBP/001_db_proto.py
+++ HBP/001_db_proto.py
@@ -3,7 +3,7 @@ Descr: db module prototype
 @author: vitkai
 Created: Sun Dec 15 2019 13:05 MSK
 """
-import __main__ as main
+import __main__
 import logging
 from os import path
 import sqlite3
@@ -11,7 +11,7 @@ from sqlite3 import Error
 
 def logging_setup():
     logger = logging.getLogger(__name__)
-    filename = path.splitext(main.__file__)[0] + '.log'
+    filename = path.splitext(__main__.__file__)[0] + '.log'
     handler = logging.FileHandler(filename)
 
     logger.setLevel(logging.DEBUG)
@@ -40,23 +40,64 @@ def create_connection(db_file):
     except Error as e:
         print(e)
         logger.error(e)
-    finally:
-        if conn:
-            conn.close()
+            
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+        logger.error(e)
 
 # main starts here
-if __name__ == "__main__":
+def main():
+    global logger
     logger = logging_setup()
 
     # get script path
     full_path, filename = path.split(path.realpath(__file__))
     logger.debug("Full path: {0} | filename: {1}".format(full_path, filename))
     
-    # print('Real path: {0}'.format(path.realpath(__file__)))
+    # db statements
+    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        begin_date text,
+                                        end_date text
+                                    ); """
+ 
+    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
+                                    id integer PRIMARY KEY,
+                                    name text NOT NULL,
+                                    priority integer,
+                                    status_id integer NOT NULL,
+                                    project_id integer NOT NULL,
+                                    begin_date text NOT NULL,
+                                    end_date text NOT NULL,
+                                    FOREIGN KEY (project_id) REFERENCES projects (id)
+                                );"""
 
-    # db access
-    create_connection(full_path + '\\' + 'hbp.db')
+    # create a database connection
+    conn = create_connection(full_path + '\\' + 'hbp.db')
 
+    # create tables
+    if conn is not None:
+        # create projects table
+        create_table(conn, sql_create_projects_table)
+ 
+        # create tasks table
+        create_table(conn, sql_create_tasks_table)
 
     logger.debug("That's all folks")
     print("\nThat's all folks")
+
+if __name__ == "__main__":
+    main()
