2bb79c2a87a42883371b86966fc86c0a29c031b1
Corvit <corvit.christian@gmail.com>
2020-01-04 10:22:30 +0100

    HBP: hbpsite: models updated

--- HBP/hbpsite/hbpapp/models.py
+++ HBP/hbpsite/hbpapp/models.py
@@ -7,3 +7,12 @@ class Category(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return self.name
+
+class CCY(models.Model):
+    """Model representing a currency."""
+    name = models.CharField(max_length=10, help_text='Enter a short name for a currency (e.g. RUB)')
+    full_name = models.CharField(max_length=200, help_text='Enter a full name for a currency (e.g. Russian ruble)')
+    
+    def __str__(self):
+        """String for representing the Model object."""
+        return '{0} ({1})'.format(self.name, self.full_name)
