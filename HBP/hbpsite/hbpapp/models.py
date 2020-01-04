85439d2e73619611e1fd342bfa6eed977a1a0c34
Corvit <corvit.christian@gmail.com>
2020-01-04 10:12:48 +0100

    HBP: hbpsite: adding models

--- HBP/hbpsite/hbpapp/models.py
+++ HBP/hbpsite/hbpapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    """Model representing a transaction category."""
+    name = models.CharField(max_length=200, help_text='Enter a transaction category (e.g. Food)')
+    
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
