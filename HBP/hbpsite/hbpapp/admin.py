b94bd62951ba94b6a0861bd3c3da7549f4bfcd0b
Corvit Chr <corvit.christian@gmail.com>
2020-01-04 22:59:27 +0300

    HBP: hbpsite: admin part added (Fix for transactions dispay is required)

--- HBP/hbpsite/hbpapp/admin.py
+++ HBP/hbpsite/hbpapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, CCY, Transactions
 
 # Register your models here.
+admin.site.register(Transactions)
+admin.site.register(CCY)
+admin.site.register(Category)
+
+class TransactionsAdmin(admin.ModelAdmin):
+    """Administration object for Transactions models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+    """
+    list_display = ('tr_date', 'tr_direction', 'display_sum', 'CCY', 'display_category', 'Content')
+    
+#admin.site.register(TransactionsAdmin)
--- HBP/hbpsite/hbpapp/models.py
+++ HBP/hbpsite/hbpapp/models.py
@@ -29,11 +29,11 @@ class Transactions(models.Model):
     """Model representing a transaction record."""
     tr_date = models.DateField(null=False, blank=False)
     tr_time = models.DateField(null=True, blank=True)
-    Sum = models.DecimalField(max_digits=19, decimal_places=10, help_text='Enter a transaction sum (e.g. 500.0, or -50.5 for receiving)')
+    Sum = models.DecimalField(max_digits=19, decimal_places=2, help_text='Enter a transaction sum')
      
     TR_DIRS = (
-        ('+', 'Spending'),
-        ('-', 'Obtaining'),
+        ('-', 'Spending'),
+        ('+', 'Obtaining'),
     )
     
     tr_direction = models.CharField(
@@ -41,10 +41,21 @@ class Transactions(models.Model):
         choices=TR_DIRS,
         blank=True,
         default='+',
-        help_text="Whether you're spending (+) or receiving (-)")
+        help_text="Whether you're spending (-) or receiving (+)")
 
     CCY = models.ForeignKey('CCY', on_delete=models.SET_NULL, null=True)
     Category = models.ManyToManyField(Category, help_text="Select a Category for this transaction")
+    
+    def display_sum(self):
+        """Shows sum with a +/- sign"""
+        return '{}{}'.format(self.tr_direction, self.Sum)
+    
+    def display_category(self):
+        """Creates a string for the Category. This is required to display genre in Admin."""
+        return ', '.join([Category.name for cat in self.Category.all()])
+
+    display_category.short_description = 'Categories'
+    
     Content = models.CharField(max_length=200, help_text='Enter a transaction category (e.g. Food)')
   
     
@@ -54,4 +65,4 @@ class Transactions(models.Model):
     
     def __str__(self):
         """String for representing the Model object."""
-        return self.name
+        return '{0} ({1}) {2}{3} {4}'.format(self.tr_date, self.tr_direction, self.Sum, self.CCY, self.Content)
