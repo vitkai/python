e13f5ad4f96fe2632e9e89d11920c804e3f8df16
Corvit Chr <corvit.christian@gmail.com>
2020-10-30 16:34:13 +0300

    downloder script added

---
+++ downloader.py
@@ -0,0 +1,53 @@
+"""
+import urllib
+
+out = '/c/Users/corvit/Downloads/Slow_Deustsch/'
+for i in range(125, 1):
+    msg = f'i={i}'
+    print(msg)
+    pdf = 'sg' + str(i) + 'kurz.pdf'
+    url = 'https://slowgerman.com/folgen/' + pdf
+
+    file = urllib.urlopen(url)
+    with open('out' + pdf, 'wb') as output:
+        output.write(file.read())
+
+    mp3 = 'sg' + str(i) + '.mp3'
+    url = 'https://slowgerman.com/folgen/' + mp3
+
+    file = urllib.urlopen(url)
+    with open('out' + mp3, 'wb') as output:
+        output.write(file.read())
+"""
+
+
+import os
+import wget
+
+print('Beginning file download with wget module')
+out = os.path.abspath('/Users/corvit/Downloads/Slow_Deustsch/')
+
+for i in range(1, 125):
+    msg = f'i={i}'
+    print(msg)
+    pdf = 'sg' + str(i) + 'kurz.pdf'
+    url = 'https://slowgerman.com/folgen/' + pdf
+    try:
+        wget.download(url, out=out)
+        # wget.download(url)
+    except Exception as e:
+        print(e)
+
+    mp3 = 'sg' + str(i) + '.mp3'
+    url = 'https://slowgerman.com/folgen/' + mp3
+    try:
+        wget.download(url, out=out)
+        # wget.download(url)
+    except Exception as e:
+        print(e)
+
+
+"""
+https://slowgerman.com/folgen/sg125kurz.pdf
+https://slowgerman.com/folgen/sg125.mp3
+"""
