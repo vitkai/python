859499c371c5964358eae24254a508b0be66b3bc
vitkai <39426330+vitkai@users.noreply.github.com>
2025-02-09 22:00:27 +0100

    Create mig21-farmsim.diff.lua

---
+++ gms/DCS/mig21-farmsim.diff.lua
@@ -0,0 +1,100 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2003cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_Z",
+				},
+			},
+			["name"] = "Rudder",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_RZ",
+				},
+			},
+		},
+		["a2004cdnil"] = {
+			["name"] = "Thrust",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_Z",
+				},
+			},
+		},
+		["a73cdnil"] = {
+			["name"] = "Wheel Brakes Lever",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_SLIDER2",
+				},
+			},
+		},
+	},
+	["keyDiffs"] = {
+		["d1035pnilu1037cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN29",
+				},
+			},
+			["name"] = "Throttle Levers - UP",
+		},
+		["d1036pnilu1037cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN30",
+				},
+			},
+			["name"] = "Throttle Levers - DOWN",
+		},
+		["d145pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN27",
+				},
+			},
+			["name"] = "Flaps Down",
+		},
+		["d146pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN28",
+				},
+			},
+			["name"] = "Flaps Up",
+		},
+		["d3064pnilunilcd8vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN31",
+				},
+			},
+			["name"] = "SAU On/Off",
+		},
+		["d3191pnilu3191cd42vd1vpnilvu0"] = {
+			["name"] = "Fire Gun",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+		},
+		["d7pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN23",
+				},
+			},
+			["name"] = "F1 Cockpit view",
+		},
+		["d8pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN24",
+				},
+			},
+			["name"] = "F2 Aircraft view",
+		},
+	},
+}
+return diff
